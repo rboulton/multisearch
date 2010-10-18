883e8be54853323dccfca9cb54fc8b65ee3f7173
Richard Boulton <richard@tartarus.org>
2010-10-18 16:19:50 +0100

    Add LazyJsonObject and tests

--- multisearch/utils.py
+++ multisearch/utils.py
@@ -33,3 +33,48 @@ safe_backend_name_re = re.compile(r'^[a-z][a-z_]*$')
 
 def is_safe_backend_name(val):
     return safe_backend_name_re.match(val) is not None
+
+class LazyJsonObject(object):
+    """This behaves like a dict and lazily converts contents to and from JSON.
+
+    """
+    def __init__(self, json=None, data=None):
+        if json is None and data is None:
+            data = {}
+        self._json = json
+        self._data = data
+
+    def _load(self):
+        """Load the data from json format, if required"""
+        if self._data is None:
+            self._data = json.loads(self._json)
+
+    def _dump(self):
+        """Dump the data to json format, if required"""
+        if self._json is None:
+            self._json = json.dumps(self._data, separators=(',', ':'))
+
+    def __getitem__(self, key):
+        self._load()
+        return self._data[key]
+
+    def __setitem__(self, key, value):
+        self._load()
+        self._json = None
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        self._load()
+        self._json = None
+        del self._data[key]
+
+    @property
+    def json(self):
+        """Get the data as a JSON string."""
+        self._dump()
+        return self._json
+
+    def items(self):
+        self._load()
+        for kv in self._data.iteritems():
+            yield kv
--- unittests/utils_tests.py
+++ unittests/utils_tests.py
@@ -15,5 +15,20 @@ class UtilsTest(unittest.TestCase):
         self.assertFalse(utils.is_safe_backend_name('hello.world'))
         self.assertFalse(utils.is_safe_backend_name('_hello'))
 
+class LazyJsonObjectTest(unittest.TestCase):
+    def test_access(self):
+        """Test accessing and modifying a LazyJsonObject.
+
+        """
+        obj = utils.LazyJsonObject()
+	self.assertRaises(KeyError, obj.__getitem__, 'hi')
+        obj['hi'] = 2
+        self.assertEqual(obj['hi'], 2)
+        self.assertEqual(obj.json, '{"hi":2}')
+        self.assertEqual(tuple(obj.items()), (("hi", 2),))
+        obj['hello'] = 3
+        self.assertEqual(tuple(sorted(obj.items())), (("hello", 3), ("hi", 2)))
+        self.assertEqual(utils.json.loads(obj.json), {"hi": 2, "hello": 3})
+
 if __name__ == '__main__':
     unittest.main()
