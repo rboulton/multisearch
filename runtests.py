9142ad5d5fd4e3214dc965a7f58e900e73f6a5bc
Richard Boulton <richard@tartarus.org>
2010-12-06 18:01:22 +0000

    Restrict output to our modules

--- runtests.py
+++ runtests.py
@@ -4,11 +4,21 @@ import os
 import shutil
 import subprocess
 
-shutil.rmtree('testdb')
+try:
+    shutil.rmtree('testdb')
+except OSError: pass
 
-p = subprocess.Popen(("nosetests", "--with-coverage", "--cover-erase",
-                      "--cover-tests", "--cover-inclusive", "--with-doctest",
-                      "--doctest-extension=rst", "unittests/", "README.rst",
+p = subprocess.Popen(("nosetests",
+                      "--with-coverage",
+                      "--cover-erase",
+                      "--cover-tests",
+                      "--cover-inclusive",
+                      "--cover-package=multisearch",
+                      "--cover-package=unittests",
+                      "--with-doctest",
+                      "--doctest-extension=rst",
+                      "unittests",
+                      "README.rst",
                       "docs"))
 
 sts = os.waitpid(p.pid, 0)[1]
