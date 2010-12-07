14f1e2e8f17934879fd059797b9293e7cce1c281
Richard Boulton <richard@tartarus.org>
2010-12-07 12:41:49 +0000

    Always raise ImportError, not KeyError, for unknown backends.  Add BaseSearchClient class as a base class for search clients.

--- multisearch/client.py
+++ multisearch/client.py
@@ -22,7 +22,8 @@ r"""Base class and factory for clients.
 """
 __docformat__ = "restructuredtext en"
 
-import utils
+import multisearch.utils
+import multisearch.errors
 
 # Change to have a SearchClient, which is a factory method, and a
 # SearchClientBase, which backends subclass.  Make all the methods
@@ -36,12 +37,11 @@ _factories = {}
 def get_factory(type):
     """Get a backend factory.
 
-    Raises KeyError or ImportError if the backend isn't known or has missing
-    dependencies.
+    Raises ImportError if the backend isn't known or has missing dependencies.
 
     """
-    if not utils.is_safe_backend_name(type):
-        raise KeyError("Backend type %r not known" % type)
+    if not multisearch.utils.is_safe_backend_name(type):
+        raise ImportError("Backend type %r not known" % type)
     factory = _factories.get(type, None)
     if factory is None:
         module_name = "multisearch.backends.%s_backend" % type
@@ -52,3 +52,99 @@ def get_factory(type):
 
 def SearchClient(type, *args, **kwargs):
     return get_factory(type)(*args, **kwargs)
+
+class BaseSearchClient(object):
+    """A base search client interface.
+
+    All methods in this base class will normally be overridden by subclasses,
+    but have default implementations to provide appropriate fallback or error
+    handling behaviour if subclasses do not override them.  In particular, this
+    is intended to allow for easy handling of features which are not available
+    or not yet implemented for particular backends: for optional methods, this
+    class will raise a FeatureNotAvailableError, whereas for required methods,
+    this class will raise a NotImplementedError.
+
+    """
+
+    def close(self):
+        """Close any open resources.
+
+        """
+        raise NotImplementedError
+
+    @property
+    def document_count(self):
+        """Return the number of documents.
+
+        """
+        raise NotImplementedError
+
+    def __len__(self):
+        """Return the number of documents.
+
+        """
+        return self.document_count
+
+    def iter_documents(self):
+        """Iterate through all the documents.
+
+        """
+        raise NotImplementedError
+
+    def __iter__(self):
+        """Iterate through all the documents.
+
+        """
+        return self.iter_documents()
+
+    def get_document(self, docid):
+        """Get a document, given a document ID.
+
+        Raise KeyError if the document does not exist.
+
+        """
+        raise NotImplementedError
+
+    def document_exists(self, docid):
+        """Return True if a document with the given id exists, False if not.
+
+        """
+        raise NotImplementedError
+
+    def query(self, value, fieldname=None, *args, **kwargs):
+        """Create a basic search for the value supplied.
+
+        If fieldname is also supplied, search only for the value in that field.
+
+        """
+        raise NotImplementedError
+
+    def search(self, search):
+        """Perform a search.
+
+        The search should be an instance of multisearch.queries.Search.
+
+        """
+        raise NotImplementedError
+
+    def commit(self):
+        """Commit any changes which are currently in progress.
+
+        """
+        raise multisearch.errors.FeatureNotAvailableError
+
+    def process(self, doc):
+        """Process an incoming document into document suitable for this
+        backend.
+
+        """
+        raise multisearch.errors.FeatureNotAvailableError
+
+    def update(self, doc, docid=None, fail_if_exists=False, assume_new=False):
+        raise multisearch.errors.FeatureNotAvailableError
+
+    def delete(self, docid, fail_if_missing=False):
+        raise multisearch.errors.FeatureNotAvailableError
+
+    def destroy_database(self):
+        raise multisearch.errors.FeatureNotAvailableError
